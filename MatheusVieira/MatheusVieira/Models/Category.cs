fb200f54cca3455df274f544df919a57453f9fb8
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2017-08-15 11:06:27 -0300

    Innitial Commit

---
+++ MatheusVieira/MatheusVieira/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatheusVieira.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
